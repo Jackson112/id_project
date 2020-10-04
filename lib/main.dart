91477495b79eac4246a478ff76a574a940d56923
Jackson Lazaro <jacksonlayaroict@gmail.com>
2020-10-04 06:44:47 -0700

    The completion of Id project
    Id project is a test project during mz learnings on flutter.it is an awesome waz to understand on how to structure a profile UI ina mobile app.

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,75 @@ class IdCard extends StatelessWidget {
            backgroundColor: Colors.grey[850],
            elevation: 0.0,
          ),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/profile.jpg'),
+                radius: 50.0,
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey[700],
+              ),
+              SizedBox(height: 20.0),
+              Text('Name',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+              ),
+            SizedBox(height: 10.0),
+              Text('Jackson',
+                style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Text('Current Flutter level',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text('29',
+                style: TextStyle(
+                  color: Colors.amberAccent,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              SizedBox(height: 30.0),
+              Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10.0 ),
+                  Text('jacksonlazaroict@gmail.com',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ],
+              )
+
+
+
+
+            ],
+
+          ),
+      ),
     );
   }
 }
